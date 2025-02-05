27d20d181fc1690d38503c90f6368a14ebce04d4
AnnaShiba <102438674+AnnaShiba@users.noreply.github.com>
2025-02-05 17:28:16 -0500

    Add basic JS and CSS to the web app

--- public/index.js
+++ public/index.js
@@ -1,1 +1,41 @@
-console.log("JAVASCRIPT IS WORKING!");
+const socket = io();
+
+socket.on('message', (message) => {
+    console.log(message);
+});
+
+const currentUser = null;
+
+const loginForm = document.querySelector('#login-form');
+const loginBlock = document.querySelector('#login');
+const chatBlock = document.querySelector('#chat-app');
+
+const loginError = document.querySelector('#login-error');
+
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    fetch('/api/user/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            username: e.target.username.value,
+            password: e.target.password.value,
+        }),
+    })
+    .then((response) => {
+        if (!response.ok)
+            throw new Error('Invalid username and password');
+
+        chatBlock.classList.remove('hidden');
+        loginBlock.classList.add('hidden');
+    })
+    .catch((error) => {
+        console.log(error);
+        console.log(loginError);
+        loginError.innerHTML = error.message;
+    });
+
+});
