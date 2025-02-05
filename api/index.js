04072ef9636346203807b25a1ea8dce79dfa0951
AnnaShiba <102438674+AnnaShiba@users.noreply.github.com>
2025-02-05 16:56:43 -0500

    Add socket io on the server

--- api/index.js
+++ api/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const http = require('http');
+const socketio = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 const userRoutes = require('./users');
 const groupRoutes = require('./groupmessage');
@@ -10,13 +14,18 @@ const privateRoutes = require('./privatemessage');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use('/user', userRoutes);
-app.use('/group', groupRoutes);
-app.use('/private', privateRoutes);
+app.use('/api/user', userRoutes);
+app.use('/api/group', groupRoutes);
+app.use('/api/private', privateRoutes);
 
 app.get("/", (req, res) => res.send("COMP 3133 – Lab Test 1 by Anna"));
 
+io.on('connection', (socket) => {
+    console.log('New WebSocket connection');
+    socket.emit('message', 'Hello World!');
+});
+
 mongoose.connect(process.env.MONGODB_URI, {})
-    .then(_ => app.listen(3123, () => console.log("Server ready on port 3123 with MongoDB.")))
+    .then(_ => server.listen(3123, () => console.log("Server ready on port 3123 with MongoDB and Socket.io.")))
     .catch(error => console.log(error));
     
