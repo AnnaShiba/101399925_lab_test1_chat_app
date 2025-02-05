3b1584ab23679143f5e647720fe449efaceee662
AnnaShiba <102438674+AnnaShiba@users.noreply.github.com>
2025-02-05 18:09:22 -0500

    Joining rooms and sending group message

--- models/users.js
+++ models/users.js
@@ -3,8 +3,8 @@ const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true, maxlength: 100 },
-    firstname: { type: String, required: true, unique: true, maxlength: 100 },
-    lastname: { type: String, required: true, unique: true, maxlength: 100 },
+    firstname: { type: String, required: true, maxlength: 100 },
+    lastname: { type: String, required: true, maxlength: 100 },
     password: { type: String, required: true, maxlength: 50 },
     createon: { type: Date, default: Date.now },
 });
--- public/index.js
+++ public/index.js
@@ -2,16 +2,17 @@ const socket = io();
 
 socket.on('message', (message) => {
     console.log(message);
+    chatMessages.innerHTML += `<p><span style="color:red"></span>${message}</p>`;
 });
 
-const currentUser = null;
-
-const loginForm = document.querySelector('#login-form');
 const loginBlock = document.querySelector('#login');
 const chatBlock = document.querySelector('#chat-app');
-
 const loginError = document.querySelector('#login-error');
+const chatMessages = document.querySelector('#chat-messages');
+const roomSelector = document.querySelector('#room-selector');
+const chatWindow = document.querySelector('#chat-window');
 
+const loginForm = document.querySelector('#login-form');
 loginForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
@@ -29,6 +30,13 @@ loginForm.addEventListener('submit', (e) => {
         if (!response.ok)
             throw new Error('Invalid username and password');
 
+        return response.json();
+    })
+    .then((data) => {
+        console.log(`Logged in as ${data.username}`);
+        localStorage.setItem('currentUser', data.username);
+
+        document.querySelector('#current-user span').innerHTML = `Welcome, ${data.username}!`;
         chatBlock.classList.remove('hidden');
         loginBlock.classList.add('hidden');
     })
@@ -37,5 +45,72 @@ loginForm.addEventListener('submit', (e) => {
         console.log(loginError);
         loginError.innerHTML = error.message;
     });
+});
 
+const logoutButton = document.querySelector('#logout-button');
+logoutButton.addEventListener('click', (e) => {
+    localStorage.removeItem('currentUser');
+    chatBlock.classList.add('hidden');
+    loginBlock.classList.remove('hidden');
+});
+
+const roomDropdown = document.querySelector('#room-dropdown');
+const joinRoomButton = document.querySelector('#join-room');
+joinRoomButton.addEventListener('click', (e) => {
+    const room = roomDropdown.value;
+
+    if (!room)
+        return;
+
+    roomSelector.classList.add('hidden');
+    chatWindow.classList.remove('hidden');
+
+    localStorage.setItem('currentRoom', room);
+    socket.emit('join', room);
+});
+
+const leaveRoomButton = document.querySelector('#leave-room');
+leaveRoomButton.addEventListener('click', (e) => {
+    roomSelector.classList.remove('hidden');
+    chatWindow.classList.add('hidden');
+
+    const room = localStorage.getItem('currentRoom');
+    socket.emit('leave', room);
+    localStorage.removeItem('currentRoom');
+});
+
+
+const chatForm = document.querySelector('#chat-form');
+chatForm.querySelector('#message').addEventListener('keypress', (e) => {
+    console.log("User is typing...");
+    socket.emit('event', "User is typing...");
+});
+
+chatForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    fetch('/api/group', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            from_user: localStorage.getItem('currentUser'),
+            room: localStorage.getItem('currentRoom'),
+            message: e.target.message.value,
+        }),
+    })
+    .then((response) => {
+        if (!response.ok)
+            throw new Error('Failed to send message');
+
+        return response.json();
+    })
+    .then((response) => {
+        console.log(`Message sent to ${response.data.room}`);
+        chatMessages.innerHTML += `<p><span style="color:green">${response.data.from_user}</span>: ${response.data.message}</p>`;
+    })
+    .catch((error) => {
+        console.log(error);
+    });
 });
